1789c54a390317884477d33d4fac4aa7aacaa465
Dylan K <drkunkel7@gmail.com>
2021-09-13 15:08:09 -0500

    first attempt at stupid horrible associations(evil)

--- models/index.js
+++ models/index.js
@@ -6,5 +6,27 @@ const Comment = require(`./Comment`);
 
 // create associations (fuck those btw)
 
+// Note associations
+User.hasMany(Note, {
+    foreignKey: `user_id`
+});
+Note.belongsTo(User, {
+    foreignKey: `user_id`
+});
+
+// Comment associations
+Comment.belongsTo(User, {
+    foreignKey: `user_id`
+});
+Comment.belongsTo(Note, {
+    foreignKey: `note_id`
+});
+User.hasMany(Comment, {
+    foreignKey: `user_id`
+});
+Note.hasMany(Comment, {
+    foreignKey: `note_id`
+});
+
 
 module.exports = { User, Note, Comment };
