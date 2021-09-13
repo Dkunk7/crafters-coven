6c175b0ce60c848b79923febc097afc6fe9258d7
Dylan K <drkunkel7@gmail.com>
2021-09-13 17:28:30 -0500

    routes connected

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require(`express`).Router();
 
 // import route groups (ex: apiRoutes, homeRoutes, etc)
+const apiRoutes = require(`./api`);
 
 // router.use
+router.use(`/api`, apiRoutes);
 
-// Directs all unused routes to an error
+// Directs all incorrect routes to an error
 router.use((req, res) => {
     res.status(404).end();
 });
