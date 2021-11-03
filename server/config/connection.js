a1d429bf4d6fe81141b626c20884b9f5f90096bf
Dylan K <drkunkel7@gmail.com>
2021-11-03 13:22:15 -0500

    added connection.js functionality

---
+++ server/config/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/crafters-coven', {
+    useNewUrlParser: true,  // I don't know what any of these four things mean
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+module.exports = mongoose.connection;
