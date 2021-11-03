d20d0d897adb09953f29427747426bf48654ea33
Dylan K <drkunkel7@gmail.com>
2021-11-03 11:33:17 -0500

    typeDefs possibly complete

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,7 +13,7 @@ const typeDefs = gql`
         title: String
         noteContent: String
         isCoordinate: Boolean
-        createdAt: String
+        createdAt: String # I think this is a string because the date will be formatted into a string
         username: String
         commentCount: Int
         comments: [Comment]
@@ -25,4 +25,26 @@ const typeDefs = gql`
         createdAt: String
         username: String
     }
+
+    type Query {
+        me: User  # This is probably something I want, but I'm not completely sure
+        users: [User]
+        user(username: String!): User # ! means username is required to search for single user
+        notes(username: String): [Note] # no ! means searching by username is optional; it's an array because it could have many results
+        note(_id: ID!): Note # _id is required to search for a single note
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth # not sure how this works; not set up yet
+        addUser(username: String!, email: String!, password: String!): Auth
+        addNote(title: String!, noteContent: String!, isCoordinate: Boolean): Note # isCoordinate is not required; false by default
+        addComment(noteId: ID!, commentContent: String!): Thought # This is performed on thought? Also noteId doesn't exist anywhere AS noteId. How does this work? *Referenced deep-thoughts
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
 `;
+
+module.exports = typeDefs;
