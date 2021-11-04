0f57e144a71e4915a20549388ef45762e5873feb
Dylan K <drkunkel7@gmail.com>
2021-11-04 12:21:52 -0500

    added queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -19,3 +19,67 @@ export const QUERY_NOTES = gql`
         }
     }
 `;
+
+export const QUERY_NOTE = gql`
+    query note($id: ID!) {
+        thought(_id: $id) {
+            _id
+            title
+            noteContent
+            isCoordinate
+            createdAt
+            username
+            commentCount
+            comments {
+                _id
+                commentContent
+                createdAt
+                username
+            }
+        }
+    }
+`;
+
+export const QUERY_USER = gql`
+    query user($username: String!) {
+        user(username: $username) {
+            _id
+            username
+            email
+            isAdmin
+            notes { # username and comments are not included here
+                _id
+                title
+                noteContent
+                isCoordinate
+                createdAt
+                commentCount
+            }
+        }
+    }
+`;
+
+export const QUERY_ME = gql`
+    {
+        me { # how does 'me' work?
+            _id
+            username
+            email
+            isAdmin
+            notes {
+                _id
+                title
+                noteContent
+                isCoordinate
+                createdAt
+                commentCount
+                comments {
+                    _id
+                    commentContent
+                    createdAt
+                    username
+                }
+            }
+        }
+    }
+`;
